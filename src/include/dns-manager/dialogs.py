b0f11d194c085d3510762b06aac528c7caeb496d
David Mulder <dmulder@suse.com>
2019-07-25 16:52:55 -0600

    Disable these options until they are implemented

--- src/include/dns-manager/dialogs.py
+++ src/include/dns-manager/dialogs.py
@@ -62,8 +62,8 @@ class NewDialog:
                 Left(TextEntry(Id('fqdn'), Opt('disabled'), self.parent)),
                 Left(Label(Id('data_label'), 'IP address:')),
                 Left(TextEntry(Id('data'), '')),
-                Left(CheckBox(Id('create_ptr'), 'Create associated pointer (PTR) record')),
-                Left(CheckBox(Id('allow_update'), 'Allow any authenticated user to update DNS records with the same owner name')),
+                Left(CheckBox(Id('create_ptr'), Opt('disabled'), 'Create associated pointer (PTR) record')),
+                Left(CheckBox(Id('allow_update'), Opt('disabled'), 'Allow any authenticated user to update DNS records with the same owner name')),
                 Bottom(Right(HBox(
                     PushButton(Id('finish'), 'Add %s' % self.obj_type.capitalize()),
                     PushButton(Id('cancel'), 'Cancel')
